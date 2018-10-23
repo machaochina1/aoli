ef7bd1972b6bdbdeb3a1a02e24c8897477d12375
machaochina1 <44237826+machaochina1@users.noreply.github.com>
2018-10-23 16:13:17 +0800

    公司首页
    公司首页，helly.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="192.168.10.111",port=5000,debug=True)
